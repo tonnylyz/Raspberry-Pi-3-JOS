50c9596bba69856cb66a6084b1263ebfe85f470d
tonnylyz <me@lyzde.com>
2017-03-17 09:32:43 +0800

    Add type header

--- Utility/myrd/myrd.c
+++ Utility/myrd/myrd.c
@@ -1,19 +1,22 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
- 
+#include <stdint.h>
+#include <inttypes.h>
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Tonny LEUNG");
 MODULE_DESCRIPTION("A kernel module to read system information");
-MODULE_VERSION("0.1");
+MODULE_VERSION("0.2");
 
 static int __init MyRD_init(void){
     printk(KERN_INFO "MyRD: Started.\n");
-    register long long int coreid asm("x0");
+    uint64_t coreid;
     __asm__ __volatile__ (
-        mrs x0, mpidr_el1
-    )
-    printk("Current core id: 0x%8x\n", coreid);
+        "mrs %0, mpidr_el1"
+        : "=r" (coreid)
+    );
+    printk("Current core id: 0x" PRIx64 "\n", coreid);
     return 0;
 }
 
